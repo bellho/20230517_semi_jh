3fd671bc14fd4a1850883ba3d9ec7f80a580ea7e
bellho <1313kjh@gmail.com>
2023-08-09 12:49:11 +0900

    인코딩 필터 수정
    실행확인 print문 제거

--- source/src/main/java/semi/jh/common/filter/EcodingFilter.java
+++ source/src/main/java/semi/jh/common/filter/EcodingFilter.java
@@ -13,11 +13,9 @@ public class EcodingFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("[jhkim] filter 진짜 들어와?");
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTP-8");
-		
 		chain.doFilter(request, response);
 	}
 	
