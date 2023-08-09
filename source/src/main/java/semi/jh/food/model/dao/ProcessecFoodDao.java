33cb78c7f1b5959fee01e3b296432fda9c61be0b
bellho <1313kjh@gmail.com>
2023-08-09 12:29:38 +0900

    selectList 삭제
    매개변수 추가로 중복이 되므로 삭제

--- source/src/main/java/semi/jh/food/model/dao/ProcessecFoodDao.java
+++ source/src/main/java/semi/jh/food/model/dao/ProcessecFoodDao.java
@@ -13,38 +13,8 @@ import semi.jh.food.model.dto.ProcessecFoodDto;
 
 
 public class ProcessecFoodDao {
+
 	/**
-	
-	  * @Method Name : selectList
-	  * @작성일 : 2023. 8. 8.
-	  * @작성자 : 1313k
-	  * @변경이력 : list.jsp에서 모든 데이터를 쿼리항목을 통해 불러옴
-	  * @Method 설명 : 
-	  * @param conn
-	  * @return
-	  * @throws SQLException
-	  */
-	public List<ProcessecFoodDto> selectList(Connection conn) throws SQLException{
-		System.out.println("[ProcessecFoodDao slecetList]");
-		List<ProcessecFoodDto> result = new ArrayList<ProcessecFoodDto>();
-		String query = "select food_name, manufacturer, calories from tb_processed_food";
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		pstmt = conn.prepareStatement(query);
-		rs = pstmt.executeQuery();
-		
-		while(rs.next()) {
-			ProcessecFoodDto dto = new ProcessecFoodDto();
-			dto.setFoodName(rs.getString("food_name"));
-			dto.setManufacturer(rs.getString("manufacturer"));
-			dto.setCalories(rs.getInt("calories"));
-			result.add(dto);
-		}
-		return result;
-	}
-	
-	/**
-	
 	  * @Method Name : selectList
 	  * @작성일 : 2023. 8. 8.
 	  * @작성자 : 1313k
@@ -89,21 +59,5 @@ public class ProcessecFoodDao {
 		System.out.println("검색함" + foodName );
 		return result;
 	}
-//	public ProcessecFoodDto selectOne (Connection conn, String foodName) throws SQLException {
-//		ProcessecFoodDto result = null;
-//		String query = "select food_name, manufacturer , calories from tb_processed_food where food_code = ?";
-//		PreparedStatement pstmt = null;
-//		ResultSet rs = null;
-//		pstmt = conn.prepareStatement(query);
-//		pstmt.setString(1, foodName);
-//		rs = pstmt.executeQuery();
-//		if(rs.next()) {
-//			result = new ProcessecFoodDto(
-//					rs.getString("select food_name"), 
-//					rs.getString("manufacturer"), 
-//					rs.getInt("calories"));
-//		}
-//		System.out.println(result);
-//		return result;
-//	}
+
 }
